77c1bb5710571a5043fb180745588c52561ffc21
Eduardo Gonzalez <edu14937@gmail.com>
2023-06-01 12:29:40 +0200

    mi tercer  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-
-import Header from "./components/Header/Header";
-import Main from "./components/main/Main";
-import About from "./components/About/About";
-import Services from "./components/Services/Services";
-import TecnichSkills from "./components/Skills/TecnichSkills";
-import Footer from "./components/Footer/Footer";
-import ContactForm from "./components/Contact/ContactForm";
 import { Routes } from "./Routes";
 import { BrowserRouter } from "react-router-dom";
 
